c6c97a0068e32b8bd9228f40d406fee813884c2a
Matheus Battisti <matheusbudkewicz@gmail.com>
2024-03-12 14:39:40 -0300

    inserindo dados

--- 4_data_fetching/src/app/todos/create/page.js
+++ 4_data_fetching/src/app/todos/create/page.js
@@ -1,15 +1,36 @@
+import { db } from "@/db.js";
+import { redirect } from "next/navigation";
+
+// 1 - Criacao do form
 export default async function TodoCreate() {
   const addTodo = async (formData) => {
     "use server";
 
     console.log(formData);
+
+    // 2 - Inserindo dados no banco
+    const titulo = formData.get("titulo");
+    const descricao = formData.get("descricao");
+    const status = "pendente";
+
+    const todo = await db.todo.create({
+      data: {
+        titulo,
+        descricao,
+        status,
+      },
+    });
+
+    console.log(todo);
+
+    redirect("/");
   };
 
   return (
     <form action={addTodo} className="flex flex-col gap-4">
-      <input type="text" name="title" placeholder="Title" required />
-      <textarea name="description" placeholder="Description" required />
-      <button type="submit">Create Todo</button>
+      <input type="text" name="titulo" placeholder="Título" required />
+      <textarea name="descricao" placeholder="Descrição" required />
+      <button type="submit">Criar Todo</button>
     </form>
   );
 }
