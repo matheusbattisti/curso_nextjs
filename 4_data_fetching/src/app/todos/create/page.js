8823ce025bbe61c4df4c0e0ca0e4058b00831106
Matheus Battisti <matheusbudkewicz@gmail.com>
2024-03-12 14:24:38 -0300

    logando dados

--- 4_data_fetching/src/app/todos/create/page.js
+++ 4_data_fetching/src/app/todos/create/page.js
@@ -1,3 +1,15 @@
-export default function TodoCreate() {
-  return <div>Criação de todo</div>;
+export default async function TodoCreate() {
+  const addTodo = async (formData) => {
+    "use server";
+
+    console.log(formData);
+  };
+
+  return (
+    <form action={addTodo} className="flex flex-col gap-4">
+      <input type="text" name="title" placeholder="Title" required />
+      <textarea name="description" placeholder="Description" required />
+      <button type="submit">Create Todo</button>
+    </form>
+  );
 }
