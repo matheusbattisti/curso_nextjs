f7cdeb84eb133175b93b06ee1aba2d9804df117b
Matheus Battisti <matheusbudkewicz@gmail.com>
2024-03-12 15:12:17 -0300

    resgate de dados

--- 4_data_fetching/src/app/page.js
+++ 4_data_fetching/src/app/page.js
@@ -1,10 +1,23 @@
+import { db } from "@/db";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  // 3 - Resgatando dados do banco
+  const todos = await db.todo.findMany();
+
+  console.log(todos);
+
   return (
     <main>
       <Link href="/todos/create">Criar tarefa</Link>
       <h1>Todos!</h1>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <p>{todo.titulo}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
