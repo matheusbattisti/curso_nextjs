4523b10bdcb9acee563476018b92398fd1bd0410
Matheus Battisti <matheusbudkewicz@gmail.com>
2024-03-14 15:13:15 -0300

    ajuste

--- 4_data_fetching/src/app/todos/[id]/page.js
+++ 4_data_fetching/src/app/todos/[id]/page.js
@@ -1,7 +1,9 @@
 // 5 - pagina dinamica com request
 import { db } from "@/db";
 
-export default async function TodoShow({ id }) {
+export default async function TodoShow(props) {
+  const id = Number(props.params.id);
+
   const todo = await db.todo.findFirst({
     where: { id },
   });
