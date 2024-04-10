4c6bbe816a1fbf29deab5d1bfc8f71c42719a534
Matheus Battisti - Hora de Codar <matheusbudkewicz@gmail.com>
2024-04-10 14:47:16 -0300

    Update actions.js

--- 4_data_fetching/src/actions.js
+++ 4_data_fetching/src/actions.js
@@ -30,3 +30,42 @@ export async function addTodo(formData) {
 
   redirect("/");
 }
+
+export async function findTodoById(id) {
+  // 11 - erro backend - error.js
+  throw new Error("Ops!");
+
+  const todo = await db.todo.findFirst({
+    where: { id },
+  });
+
+  return todo;
+}
+
+export async function updateTodo(formState, formData) {
+  const id = parseInt(formData.get("id"));
+  const titulo = formData.get("titulo");
+  const descricao = formData.get("descricao");
+
+  if (titulo.length < 5) {
+    return {
+      errors: "O título precisa de pelo menos 5 caracteres.",
+    };
+  }
+
+  if (descricao.length < 10) {
+    return {
+      errors: "A descrição precisa de pelo menos 10 caracteres.",
+    };
+  }
+
+  await db.todo.update({
+    where: { id },
+    data: {
+      titulo,
+      descricao,
+    },
+  });
+
+  redirect("/");
+}
