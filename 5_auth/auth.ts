39f15e89341e1b60a2e9ea482f204abfd8d16966
Matheus Battisti <matheusbudkewicz@gmail.com>
2024-05-10 13:03:45 -0300

    auth com prisma

--- 5_auth/auth.ts
+++ 5_auth/auth.ts
@@ -3,7 +3,14 @@ import NextAuth from "next-auth";
 
 import google from "next-auth/providers/google";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const config = {
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
   providers: [google],
   callbacks: {
     authorized({ request, auth }) {
---
+++ 5_auth/src/app/middleware/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Página protegida por middleware</div>;
+};
+
+export default Page;
